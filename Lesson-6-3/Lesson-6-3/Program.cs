2236790874e43928e9058ffe0a8d081e32dc4941
Сергей <gan7er@yandex.ru>
2021-03-10 08:27:48 +0300

    Начало

---
+++ Lesson-6-3/Lesson-6-3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lesson_6_3
+{
+    class Program
+    {
+        [Serializable]
+        public class MyArraySizeException : Exception { };
+        public class MyArrayDataException : Exception
+        {
+            public MyArrayDataException(string message) : base(message)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[,] arr = { { "1", "2", "3", "4" }, { "1", "2", "3", "CV" }, { "1", "2", "3", "4" }, { "1", "2", "3", "4" } };
+            //string[,] arr = { { "1", "2", "3", "4" }, { "1", "2", "3", "4" } };
+            Console.WriteLine(ArrSum(arr));
+        }
+        static int ArrSum(string[,] a)
+        {
+            int sum = 0;
+            try
+            {
+                if (a.GetLength(0) != 4 || a.GetLength(1) != 4)
+                {
+                    throw new MyArraySizeException();
+                }
+
+                int[,] arrInt = new int[4, 4];
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    for (int i = 0; i < 4; i++)
+                    {
+
+                        if (int.TryParse(a[j, i], out int nom) == false)
+                        {
+
+                            throw new MyArrayDataException($"{j.ToString()} и {i.ToString()}");
+                        }
+                        arrInt[j, i] = Convert.ToInt32(a[j, i]);
+
+                        sum = sum + arrInt[j, i];
+                    }
+
+                }
+
+            }
+
+            catch (MyArraySizeException)
+            {
+                Console.WriteLine("Ошибка: Массив должен быть 4*4");
+            }
+            catch (MyArrayDataException e)
+            {
+                Console.WriteLine($"В массиве не число");
+                Console.WriteLine($"Ощибка по адресу массива: {e.ToString()}");
+            }
+
+
+            return sum;
+
+        }
+    }
+}
