8fb0e1ddd0ac2e4da63dff8060ad55a3071c5a2c
Сергей <gan7er@yandex.ru>
2021-03-10 08:23:16 +0300

    Конечный вариант

--- Lesson-6-4/Lesson-6-4/Program.cs
+++ Lesson-6-4/Lesson-6-4/Program.cs
@@ -10,8 +10,9 @@ namespace Lesson_6_4
             person[0] = new Personcs("Аристов Сергей Иванович", "Директор", "sorok@mail.ru", 505001, 150000, 48);
             person[1] = new Personcs("Мясников Владимир Петрович", "Менеджер", "petr@mail.ru", 505002, 100000, 30);
             person[2] = new Personcs("Алексеев Сергей Иванович", "Торговый", "aleks@mail.ru", 505003, 70000, 27);
-            person[3] = new Personcs("Мальгинов Иван Иванович", "Кладовщик", "mal@mail.ru", 505004, 50000, 39);
+            person[3] = new Personcs("Мальгинов Иван Иванович", "Кладовщик", "mal@mail.ru", 505004, 50000, 41);
             person[4] = new Personcs("Спиридонов Константин Алексеевич", "Грузчик", "spir@mail.ru", 505005, 30000, 18);
+            //Вывод 2х сотрудников
             for (int i = 0; i < person.Length; i++)
             {
                 if (person[i].Age > 40)
